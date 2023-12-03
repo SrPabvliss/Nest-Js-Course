5f028938ac7cb650d4cdec0724aed3c7fb6d577d
SrPabvliss <pablomartinvillacres@gmail.com>
2023-12-03 18:15:07 -0500

    Classes ts file modified, pokeapi-response.interface.ts created , (18) Api types autogenerated

--- 01-Typescrypt-intro/src/bases/03-classes.ts
+++ 01-Typescrypt-intro/src/bases/03-classes.ts
@@ -9,6 +9,10 @@
 // }
 
 import axios from "axios";
+import {
+  Move,
+  PokeAPIResponse,
+} from "../interfaces/pokeapi-response.interface";
 
 export class Pokemon {
   constructor(
@@ -30,9 +34,11 @@ export class Pokemon {
     console.log(`${this.name}, ${this.name} `);
   }
 
-  async getMoves() {
-    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/4`);
-    return data;
+  async getMoves(): Promise<Move[]> {
+    const { data } = await axios.get<PokeAPIResponse>(
+      `https://pokeapi.co/api/v2/pokemon/4`
+    );
+    return data.moves;
   }
 }
 
