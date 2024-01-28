95556e29a094dc5d4bcbf5cba5068f3305bb3970
SrPabvliss <pablomartinvillacres@gmail.com>
2024-01-28 18:01:02 -0500

    Deployment preparation

--- 03-pokedex-api/src/app.module.ts
+++ 03-pokedex-api/src/app.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
+
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { env_config } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({
+      load: [ env_config ],
+      validationSchema: JoiValidationSchema,
+    }),
+    ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..','public'),
     exclude: ['/api/(.*)'],
   }), 
-  MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+  MongooseModule.forRoot(process.env.MONGO_DB,{
+    dbName: 'pokemons-nest'
+  }),
   PokemonModule,
   CommonModule,
   SeedModule, 
